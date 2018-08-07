957f2613ad5c30cad92e838d77c14692e1023dcc
Tony Hsu <tonyc.t.hsu@gmail.com>
2018-08-07 22:18:33 +0800

    Implement

--- dex_sync.gemspec
+++ dex_sync.gemspec
@@ -9,19 +9,19 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Tony Hsu"]
   spec.email         = ["tonyc.t.hsu@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{DexSync is an automated tool for updating configuration from DexApp}
+  # spec.description   = %q{TODO: Write a longer description or delete this line.}
+  # spec.homepage      = "TODO: Put your gem's website or public repo URL here."
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
-  if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
-  else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
-  end
+  # if spec.respond_to?(:metadata)
+  #   spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+  # else
+  #   raise "RubyGems 2.0 or newer is required to protect against " \
+  #     "public gem pushes."
+  # end
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -35,4 +35,9 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"
+
+  spec.add_dependency 'faraday', '~> 0.14.0'
+  spec.add_dependency 'faraday_middleware', '~> 0.12.2'
+  spec.add_dependency 'faraday-cookie_jar', '~> 0.0.6'
+  spec.add_dependency 'nokogiri', '~> 1.8.2'
 end
--- lib/dex_sync.rb
+++ lib/dex_sync.rb
@@ -1,5 +1,115 @@
-require "dex_sync/version"
+require 'dex_sync/version'
+require 'faraday'
+require 'faraday_middleware'
+require 'faraday-cookie_jar'
+require 'nokogiri'
+require 'yaml'
 
 module DexSync
-  # Your code goes here...
+  class Config
+    def initialize
+      @configuration = YAML.safe_load(File.read(File.expand_path('~/dex_sync.yaml')))
+    end
+
+    def dex
+      @configuration.fetch('DEX')
+    end
+
+    def github
+      @configuration.fetch('GITHUB')
+    end
+
+    def namespaces
+      @configuration.fetch('NAMESPACES')
+    end
+
+    def clusters
+      @configuration.fetch('CLUSTERS')
+    end
+
+    def user_session
+      @configuration.fetch('USER_SESSION')
+    end
+
+    def gh_session
+      @configuration.fetch('GH_SESSION')
+    end
+  end
+
+  class UpdateKubeConfigService
+    def initialize(config)
+      @config = config
+    end
+
+    def perform
+      @config.clusters.each do |cluster|
+        login_params = { cross_client: cluster, offline_access: 'yes' }
+
+        puts("Logging into dex app...#{cluster}")
+        response = connection.post('login', login_params)
+        puts('Wait for it...')
+
+        doc = Nokogiri::HTML(response.body)
+
+        refresh_token = doc.css('form > input[name=refresh_token]').first['value']
+        id_token      = doc.css('form > input[name=id_token]').first['value']
+
+        @config.namespaces.each do |namespace|
+          puts("Dowloading config for #{cluster}:#{namespace}")
+
+          download_params = {
+            refresh_token: refresh_token,
+            id_token: id_token,
+            namespace: namespace,
+            internal: true
+          }
+
+          response = connection.post('download', download_params)
+
+          config = YAML.safe_load(response.body)
+
+          File.open(File.expand_path("~/.kubeconfigs/#{cluster}-#{namespace}-internal"), 'w') do |f|
+            f.write(config.to_yaml)
+          end
+        end
+      end
+    end
+
+    private
+
+    def connection
+      @connection ||= Faraday.new(url: @config.dex) do |f|
+        f.use :cookie_jar
+        f.use FaradayMiddleware::FollowRedirects, limit: 10
+        f.request  :url_encoded
+        f.adapter  Faraday.default_adapter
+        f.headers['Cookie'] = HTTP::Cookie.cookie_value(cookie_jar.cookies)
+      end
+    end
+
+    def cookie_jar
+      HTTP::CookieJar.new.tap do |jar|
+        jar.add(user_session_cookie)
+        jar.add(gh_session_cookie)
+      end
+    end
+
+    def user_session_cookie
+      HTTP::Cookie.new(
+        name: 'user_session',
+        value: @config.user_session,
+        domain: @config.github,
+        path: '/'
+      )
+    end
+
+    def gh_session_cookie
+      HTTP::Cookie.new(
+        name: '_gh_sess',
+        value: @config.gh_session,
+        domain: @config.github,
+        path: '/'
+      )
+    end
+  end
 end
